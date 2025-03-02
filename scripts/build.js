c0a3beb175b72bb6e2ab75dfa7ea12f74a1b3b89
Ulysse ARNAUD <ulysse@MacBook-Pro-de-Ulysse.local>
2025-03-02 13:37:19 +0100

    updated css in js in html to 0.3.4

--- scripts/build.js
+++ scripts/build.js
@@ -1,29 +1,26 @@
-// read file index.html
-// we are not going to compress it
-// we are going to minify it ourselves
-
 const fs = require('fs');
-const process = require('process');
 
 const source = fs.readFileSync('src/index.html', 'utf8');
 const destination = 'index.html';
 
 
 const minify = (source) => {
-    console.log('Minifying index.html');
-
     let minified = "";
 
     minified = source.trim().replace(/>\s+</g, '><');
     
-    const tags = Array.from(minified.match(/<(?!\/)[a-zA-Z0-9"'=,\[\]{}()>\/%:.\-\s]+>+/g));
-
     // remove spaces between attributes
     minified = minified.replace(/([a-zA-Z0-9\-]+=['"][a-zA-Z0-9\[\]()%.\s\-\/]+['"])\s*/g, '$1');
     
     // remove spaces between attributes and tags
     minified = minified.replace(/class="([^"]+)"/g, (match, classes) => {
-        return `class="${classes.replace(/\s+/g, ' ').trim()}"`;
+        let css;
+        //remove comments (// or /* */)
+        classes = classes.replace(/\/\*[\s\S]*?\*\//g, '');
+        classes = classes.replace(/\/\/.*/g, '');
+        // remove spaces
+        css = classes.replace(/\s+/g, ' ').trim();
+        return `class="${css}"`;
     });
 
     // optimize text inside tags (text nodes)
@@ -37,7 +34,7 @@ const minify = (source) => {
         css = css.replace(/\/\/.*/g, '');
         // remove spaces
         css = css.replace(/\s+/g, ' ').trim();
-        
+
         return `<style>${css}</style>`;
     });
 
