936bf1700f3c2caff99e3a17a29dac5ba152f989
Ulysse ARNAUD <ulysse@MacBook-Pro-de-Ulysse.local>
2025-03-03 22:37:04 +0100

    Fixed print

--- scripts/build.js
+++ scripts/build.js
@@ -7,43 +7,43 @@ const destination = 'index.html';
 
 
 const minify = async (source) => {
-    let minified = "";
+    let minified = source.trim();
 
-    minified = source.trim().replace(/>\s+</g, '><');
+    minified = minified.replace(/>\s+</g, '><');
     
     // remove spaces between attributes
-    minified = minified.replace(/([a-zA-Z0-9\-]+=['"][a-zA-Z0-9\[\]()%.\s\-\/]+['"])\s*/g, '$1');
+    //minified = minified.replace(/([a-zA-Z0-9\-]+=['"][a-zA-Z0-9\[\]()%.\s\-\/]+['"])\s*/g, '$1');
     
     // remove spaces between attributes and tags
-    minified = minified.replace(/class="([^"]+)"/g, (match, classes) => {
-        let css;
-        //remove comments (// or /* */)
-        classes = classes.replace(/\/\*[\s\S]*?\*\//g, '');
-        classes = classes.replace(/\/\/.*/g, '');
-        // remove spaces
-        css = classes.replace(/\s+/g, ' ').trim();
-        return `class="${css}"`;
-    });
+    // minified = minified.replace(/class="([^"]+)"/g, (match, classes) => {
+    //     let css;
+    //     //remove comments (// or /* */)
+    //     classes = classes.replace(/\/\*[\s\S]*?\*\//g, '');
+    //     classes = classes.replace(/\/\/.*/g, '');
+    //     // remove spaces
+    //     css = classes.replace(/\s+/g, ' ').trim();
+    //     return `class="${css}"`;
+    // });
 
-    // optimize text inside tags (text nodes)
-    minified = minified.replace(/>\s+(\S)/g, '>$1');
-    minified = minified.replace(/(\S)\s+</g, '$1<');
+    // // optimize text inside tags (text nodes)
+    // minified = minified.replace(/>\s+(\S)/g, '>$1');
+    // minified = minified.replace(/(\S)\s+</g, '$1<');
 
     // minify css
-    minified = minified.replace(/<style>([a-zA-Z0-9:.>()!,{}@%/\[\];\-='"\s]+)<\/style>/g, (match, css) => {
-        // remove comments
-        css = css.replace(/\/\*[\s\S]*?\*\//g, '');
-        css = css.replace(/\/\/.*/g, '');
-        // remove spaces
-        css = css.replace(/\s+/g, ' ').trim();
+    // minified =< minified.replace(/<style>([a-zA-Z0-9:.>()!,{}@%/\[\];\-='"\s]+)<\/style>/g, (match, css) => {
+    //     // remove comments
+    //     css = css.replace(/\/\*[\s\S]*?\*\//g, '');
+    //     css = css.replace(/\/\/.*/g, '');
+    //     // remove spaces
+    //     css = css.replace(/\s+/g, ' ').trim();
 
-        return `<style>${css}</style>`;
-    });
+    //     return `<style>${css}</style>`;
+    // });
 
     // minify js
-    minified = minified.replace(/<script>([\S\s]+)<\/script>/g, (match, js) => {
-        return `<script>${js.replace(/\s+/g, ' ').trim()}</script>`;
-    });
+    // minified = minified.replace(/<script>([\S\s]+)<\/script>/g, (match, js) => {
+    //     return `<script>${js.replace(/\s+/g, ' ').trim()}</script>`;
+    // });
 
     // transform images to base64
     const imgMatches = [...minified.matchAll(/<img[a-zA-Z0-9"'=\s]+src="([^"]+)"/g)];
